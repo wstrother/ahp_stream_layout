59fa5ce432556520d2adb86faebfd145581f1235
Wyatt Strother <wyattstrother@gmail.com>
2019-12-12 19:01:38 -0500

    prepared master branch for public use

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const routes = require('./routes');
 
+// run app on your own machine
 const port = 4000;
 const ip = '127.0.0.1';
 
-http.createServer(routes.handleRequest).listen(port, ip);
 
+// run app on LAN
+// const port = {YOUR PORT NUMBER};
+// const ip = {YOUR INTERNAL IP};
+
+// launch server
+http.createServer(routes.handleRequest).listen(port, ip);
 console.log(`listening on ${ip}:${port}`);
---
+++ readme.md
@@ -0,0 +1,3 @@
+A very simple node app that can be used as a browser source to provide layouts for OBS.
+
+Better readme coming soon!
